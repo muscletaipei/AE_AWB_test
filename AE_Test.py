452eb6e9811b4a03ea5f9f3dbf45c38c7f55fbda
MSI\joelin <muscletaipei@gmail.com>
2024-03-29 16:36:08 +0800

    first commit

---
+++ AE_Test.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from tkinter import Tk, filedialog
+from tqdm import tqdm
+import shutil
+import time
+timestr = time.strftime("%Y%m%d") #%Y%m%d%H%M%S
+
+
+def extract_frames(video_path, output_folder):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error: Could not open the video file.")
+        return
+    
+    # Get video properties
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = total_frames / fps
+    
+    # Create output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+    
+    # Initialize lists to store brightness values and time in seconds
+    brightness_values = []
+    times = []
+    
+    # Start capturing frames
+    frame_count = 0
+    progress_bar = tqdm(total=total_frames, desc="Extracting Frames", unit="frames")
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        # Convert frame to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # Calculate brightness of the frame
+        brightness = np.mean(gray_frame)
+        brightness_values.append(brightness)
+        
+        # Calculate time in seconds
+        time_sec = frame_count / fps
+        times.append(time_sec)
+        
+        # Save the frame as image (HD resolution)
+        frame_filename = f"{output_folder}/frame_{frame_count}.jpg"
+        cv2.imwrite(frame_filename, frame)
+        
+        frame_count += 1
+        progress_bar.update(1)
+    
+    # Release the video capture object
+    cap.release()
+    progress_bar.close()
+    print(f"Frames extracted: {frame_count}/{total_frames}")
+    
+    # Plot brightness values
+    plt.plot(times, brightness_values)
+    plt.title('AE_Test')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Brightness')
+    plt.savefig(f"AE_Test.png")
+    plt.show()
+    plt.close()
+
+def select_video_file():
+    root = Tk()
+    root.withdraw()
+    video_path = filedialog.askopenfilename(title="Select a video file")
+    return video_path
+
+if __name__ == "__main__":
+    
+    print("Press Enter when you're ready to select the video file.")
+    
+    # Prompt user to select a video file using a GUI
+    time.sleep(1)
+    video_path = select_video_file()
+    
+    # Set output folder in temp directory
+    output_folder = os.path.join("temp", timestr)
+    
+    # Call the function with user input
+    extract_frames(video_path, output_folder)
+    
+    print("Delete 'temp' folder now.....")
+    # Remove temporary folder and its contents
+    shutil.rmtree("temp")
